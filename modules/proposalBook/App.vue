7f7563ed887e9ca473d8a43e1a2b448b7c348c4d
xuyuanliang <>
2020-03-26 15:54:52 +0800

    7

--- modules/proposalBook/App.vue
+++ modules/proposalBook/App.vue
@@ -176,7 +176,8 @@ export default {
       }
     },
     getUserInfo(value) {
-      console.log("%c 监听到 getUserInfo 改变xxxxx", "color:#FF00FF;", value);
+      console.log("%c 监听到 getUserInfo 改变xxxxx", "color:#FF00FF;", value         );
+      console.log("%c 监听到 getUserInfo 改变xxxxx", "color:#FF00FF;", kkkkk         );
       if (!window.utils.utilsPlugin.isEmptyObject(value)) {
         this.showlogin = false;
       }
