277e8be0548522b8b67b8d042abaa874cdcbfd9a
xuyuanliang <>
2020-03-26 15:24:34 +0800

    测试

--- modules/proposalBook/App.vue
+++ modules/proposalBook/App.vue
@@ -176,7 +176,8 @@ export default {
       }
     },
     getUserInfo(value) {
-      console.log("%c 监听到 getUserInfo 改变xxxx", "color:#FF00FF;",   value);
+      console.log("%c 监听到 getUserInfo 改变xxxx", "color:#FF00FF;",   value    );
+      console.log("%c 监听到 getUserInfo 改变xxxx", "color:#FF00FF;",   宝贝宝贝);
       if (!window.utils.utilsPlugin.isEmptyObject(value)) {
         this.showlogin = false;
       }
