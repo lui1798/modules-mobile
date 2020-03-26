a1a23cbd73e7715df5184f19e4bec2aca89dd654
xuyuanliang <>
2020-03-26 15:19:11 +0800

    Revert "测试lint-staged"
    This reverts commit 669863517b6e0f67b7a2a0b41e71c4ea00b1067a.

--- modules/proposalBook/App.vue
+++ modules/proposalBook/App.vue
@@ -176,7 +176,7 @@ export default {
       }
     },
     getUserInfo(value) {
-      console.log("%c 监听到 getUserInfo 改变xxxx", "color:#FF00FF;",       value    );
+      console.log("%c 监听到 getUserInfo 改变xxxx", "color:#FF00FF;",   value);
       if (!window.utils.utilsPlugin.isEmptyObject(value)) {
         this.showlogin = false;
       }
